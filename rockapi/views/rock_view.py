8b9552f6df8b6c30c17efd7f9017b743778ba3a7
Blake Jon Mycal Smith <B.jonmycal@gmail.com>
2024-06-13 15:43:25 -0400

    rock auth

--- rockapi/views/rock_view.py
+++ rockapi/views/rock_view.py
@@ -11,27 +11,64 @@ class RockView(ViewSet):
 
 
     def create(self, request):
-        """Handle POST operations
+        """Handle POST requests for rocks
 
         Returns:
-            Response -- JSON serialized instance
+            Response: JSON serialized representation of newly created rock
         """
 
-        # You will implement this feature in a future chapter
-        return Response("", status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        # Get an object instance of a rock type
+        chosen_type = Type.objects.get(pk=request.data['typeId'])
+
+        # Create a rock object and assign it property values
+        rock = Rock()
+        rock.user = request.auth.user
+        rock.weight = request.data['weight']
+        rock.name = request.data['name']
+        rock.type = chosen_type
+        rock.save()
+
+        serialized = RockSerializer(rock, many=False)
+
+        return Response(serialized.data, status=status.HTTP_201_CREATED)
 
     def list(self, request):
         """Handle GET requests for all items
-
+        
         Returns:
             Response -- JSON serialized array
         """
+        # get query string parameter
+        owner_only = self.request.query_params.get("owner", None)
+
         try:
             rocks = Rock.objects.all()
+            if owner_only is not None and owner_only == "current":
+                rocks = rocks.filter(user=request.auth.user)
             serializer = RockSerializer(rocks, many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
         except Exception as ex:
             return HttpResponseServerError(ex)
+        
+    def destroy(self, request, pk=None):
+        """Handle DELETE requests for a single item
+
+        Returns:
+            Response -- 200, 404, or 500 status code
+        """
+        try:
+            rock = Rock.objects.get(pk=pk)
+            if rock.user.id == request.auth.user.id:
+                rock.delete()
+                return Response(None, status=status.HTTP_204_NO_CONTENT)
+            else:
+                return Response({'message': 'You do not own that rock'}, status=status.HTTP_403_FORBIDDEN)
+
+        except Rock.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR) 
 
 class RockTypeSerializer(serializers.ModelSerializer):
     """JSON serializer"""
@@ -46,6 +83,7 @@ class RockOwnerSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ( 'first_name', 'last_name' )
+
 
 class RockSerializer(serializers.ModelSerializer):
     """JSON serializer"""
